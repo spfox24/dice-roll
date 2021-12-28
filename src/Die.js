2469d2dec18919aa31b09e2e77f44def7e637435
Steven Fox <StevenKaty@Stevens-MacBook-Air.local>
2021-12-28 11:02:37 -0800

    first commit

---
+++ src/Die.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './Die.css'
+
+class Die extends Component {
+    render() {
+        const { face, rolling } = this.props
+
+        // Using font awesome icon to show dots
+        return <i className={`Die fas fa-dice-${face}
+            ${rolling && 'Die-shaking'}`} />
+    };
+};
+
+export default Die;
